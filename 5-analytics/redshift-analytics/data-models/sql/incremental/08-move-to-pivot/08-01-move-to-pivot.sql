a514a96f1ea0043408c8276eba971967da7894ca
Christophe Bogaert <christophe@snowplowanalytics.com>
2015-03-20 09:24:54 +0000

    Consolidated the move-to-pivot queries into one file

--- 5-analytics/redshift-analytics/data-models/sql/incremental/08-move-to-pivot/08-01-move-to-pivot.sql
+++ 5-analytics/redshift-analytics/data-models/sql/incremental/08-move-to-pivot/08-01-move-to-pivot.sql
@@ -88,3 +88,70 @@ BEGIN;
     LEFT JOIN snowplow_intermediary.sessions_final_frame AS l ON b.domain_userid = l.domain_userid AND b.domain_sessionidx = l.domain_sessionidx
   );
 COMMIT;
+
+-- Events belonging to the same visitor can arrive at different times and could end up in different batches.
+-- Rows in the visitors_new table therefore have to be merged with those in the pivot table.
+
+-- Move the consolidated visitors to the pivot table (but first delete its contents).
+
+BEGIN;
+  DELETE FROM snowplow_pivots.visitors;
+
+  INSERT INTO snowplow_pivots.visitors (
+    SELECT
+      b.blended_user_id,
+      b.first_touch_tstamp,
+      b.last_touch_tstamp,
+      b.dvce_min_tstamp,
+      b.dvce_max_tstamp,
+      b.max_etl_tstamp,
+      b.event_count,
+      b.session_count,
+      b.page_view_count,
+      b.time_engaged_with_minutes,
+      f.landing_page_host,
+      f.landing_page_path,
+      f.mkt_source,
+      f.mkt_medium,
+      f.mkt_term,
+      f.mkt_content,
+      f.mkt_campaign,
+      f.refr_source,
+      f.refr_medium,
+      f.refr_term,
+      f.refr_urlhost,
+      f.refr_urlpath
+    FROM      snowplow_intermediary.visitors_aggregate_frame AS b
+    LEFT JOIN snowplow_intermediary.visitors_initial_frame AS f ON b.blended_user_id = f.blended_user_id
+  );
+COMMIT;
+
+-- Events belonging to the same page view can arrive at different times and could end up in different batches.
+-- Rows in the page_views_new table therefore have to be merged with those in the pivot table.
+
+-- Move the consolidated page views to the pivot table (but first delete its contents).
+
+BEGIN;
+  DELETE FROM snowplow_pivots.page_views;
+
+  INSERT INTO snowplow_pivots.page_views (
+    SELECT
+      blended_user_id,
+      inferred_user_id,
+      domain_userid,
+      domain_sessionidx,
+      page_urlhost,
+      page_urlpath,
+      MIN(first_touch_tstamp) AS first_touch_tstamp,
+      MAX(last_touch_tstamp) AS last_touch_tstamp,
+      MIN(dvce_min_tstamp) AS dvce_min_tstamp, -- Used to replace SQL window functions
+      MAX(dvce_max_tstamp) AS dvce_max_tstamp, -- Used to replace SQL window functions
+      MAX(max_etl_tstamp) AS max_etl_tstamp, -- Used for debugging
+      SUM(event_count) AS event_count,
+      SUM(page_view_count) AS page_view_count,
+      SUM(page_ping_count) AS page_ping_count,
+      SUM(time_engaged_with_minutes) AS time_engaged_with_minutes
+    FROM snowplow_intermediary.page_views_new
+    GROUP BY 1,2,3,4,5,6
+  );
+COMMIT;
